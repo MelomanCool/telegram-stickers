81c27a5f22612f07103c6c4bb5573f51a28762d7
MelomanCool <melomancool@gmail.com>
2017-12-18 23:07:48 +0400

    Refactor /delete command

--- handlers/_delete.py
+++ handlers/_delete.py
@@ -7,20 +7,19 @@ sticker_storage = model.get_storage()
 
 @inject_quoted_sticker_id
 def delete(_, update, quoted_sticker_id):
-    """Deletes a meme by voice file"""
+    """Deletes a sticker"""
 
     message = update.message
 
     try:
-        meme_name = sticker_storage.get_by_file_id(quoted_sticker_id).name
-    except KeyError:
-        message.reply_text("I don't know that meme, sorry.")
-        return
-
-    try:
         sticker_storage.delete_by_file_id(quoted_sticker_id, message.from_user.id)
-    except Unauthorized:
-        message.reply_text("Sorry, you can only delete the memes you added yourself.")
+
+    except KeyError:
+        message.reply_text("I don't know that sticker, sorry.")
         return
 
-    message.reply_text('The meme "{name}" has been deleted.'.format(name=meme_name))
+    except Unauthorized:
+        message.reply_text("Sorry, you can only delete the stickers you added yourself.")
+        return
+
+    message.reply_text('The sticker has been deleted.')
