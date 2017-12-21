0ec6e3a6d83a359f410fbe9ed69c31079f9fddf8
MelomanCool <melomancool@gmail.com>
2017-12-21 21:08:47 +0400

    Switch to pytest

--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
+import pytest
+
 from model.sticker_storage import SqliteStickerStorage
 
 
-def test_that_get_tag_returns_added_tags():
+@pytest.fixture
+def sticker_storage():
+    return SqliteStickerStorage(':memory:')
+
+
+def test_that_get_tag_returns_added_tags(sticker_storage):
     tags = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
     file_id = '1'
 
-    sticker_storage = SqliteStickerStorage(':memory:')
     sticker_storage.add(
         file_id=file_id,
         tags=tags,
@@ -14,7 +20,3 @@ def test_that_get_tag_returns_added_tags():
     sticker = sticker_storage.get_by_file_id(file_id)
 
     assert set(tags) == set(sticker_storage.get_tags(sticker.id))
-
-
-if __name__ == '__main__':
-    test_that_get_tag_returns_added_tags()
