fa081af06b79582ca3f82992c21120c4961e3393
MelomanCool <melomancool@gmail.com>
2018-03-08 21:57:46 +0400

    Remove old cached results when they expire and tweak logging format a bit

--- handlers/_inline.py
+++ handlers/_inline.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from telegram import InlineQueryResultCachedSticker
 
 import model
@@ -13,12 +15,18 @@ PAGE_SIZE = 50
 large_results = {}
 
 
+def filter_old_results(results):
+    now = datetime.now()
+    return {q:res for q,res in results.items()
+            if res['expires'] > now}
+
+
 def inlinequery(_, update):
+    global large_results
+
     inl_q = update.inline_query
     query = inl_q.query
     offset = int(inl_q.offset) if inl_q.offset else 0
-
-    logger.info('Inline query: %s', query)
 
     if offset == 0 or query not in large_results:
         if query:
@@ -27,14 +35,18 @@ def inlinequery(_, update):
             stickers = sticker_storage.get_most_popular()
 
         if len(stickers) > PAGE_SIZE:
-            large_results[query] = stickers
+            large_results[query] = {'stickers': stickers,
+                                    'expires': datetime.now() + timedelta(minutes=5)}
 
     else:
-        stickers = large_results[query]
+        stickers = large_results[query]['stickers']
+
+    large_results = filter_old_results(large_results)
 
     have_next_page = bool(len(stickers) - offset > PAGE_SIZE)
     next_offset=str(offset+PAGE_SIZE) if have_next_page else ''
-    logger.info('Len: %s, offset: %d, has next page: %d, next offset: %s', len(stickers), offset, have_next_page, next_offset)
+    logger.info('Query: %s, stickers: %s, offset: %d, has next page: %d, next offset: %s',
+                query, len(stickers), offset, have_next_page, next_offset)
 
     results = [
         InlineQueryResultCachedSticker(sticker.id, sticker.file_id)
