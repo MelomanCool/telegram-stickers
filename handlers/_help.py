5dde50828e548a9a03b96ff35bdb74b1fb402047
MelomanCool <melomancool@gmail.com>
2018-08-02 00:45:42 +0400

    Update help to be suitable for groups also

--- handlers/_help.py
+++ handlers/_help.py
@@ -2,6 +2,6 @@ def help_(_, update):
     update.message.reply_text(
         'This bot can remember stickers to help you find them more easily.\n'
         '\n'
-        'To add a sticker, just send it here. To manage sticker\'s tags,'
-        ' reply to a sticker with commands /add_tags or /delete_tag.'
+        'To add a sticker, just send it to me in private. To manage sticker\'s'
+        ' tags, reply to a sticker with commands /add_tags or /delete_tag.'
     )
