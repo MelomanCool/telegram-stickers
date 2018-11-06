db049f5b40ff1a3d9ed2a025d27a27038ce61865
MelomanCool <melomancool@gmail.com>
2018-11-06 23:53:08 +0400

    user_ids filter

--- custom_filters.py
+++ custom_filters.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from telegram.ext import BaseFilter
 
 import model
@@ -12,3 +14,11 @@ class IsInDatabase(BaseFilter):
 
 
 is_in_database = IsInDatabase(storage=model.get_storage())
+
+
+def user_ids(user_ids, func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        if update.message.from_user.id in user_ids:
+            return func(bot, update, *args, **kwargs)
+    return wrapped
