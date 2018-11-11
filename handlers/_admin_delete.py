864f01bca75372baf5faf5ca507e1849839cba2f
MelomanCool <melomancool@gmail.com>
2018-11-11 21:39:02 +0400

    Whoops

--- handlers/_admin_delete.py
+++ handlers/_admin_delete.py
@@ -10,7 +10,5 @@ logger = logzero.setup_logger(__name__)
 
 @inject_quoted_sticker_id
 def admins_delete(_, update, quoted_sticker_id):
-    sticker_storage.delete_by_file_id_without_check(
-        sticker_storage.get_by_file_id(quoted_sticker_id).id
-    )
+    sticker_storage.delete_by_file_id_without_check(quoted_sticker_id)
     update.message.reply_text('Sticker have been deleted.')
