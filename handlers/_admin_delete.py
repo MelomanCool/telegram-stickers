c03b284e30f9dc15d7c616a374d9d558b62731fd
MelomanCool <melomancool@gmail.com>
2018-11-11 21:41:36 +0400

    Fix: admins_delete should be admin_delete

--- handlers/_admin_delete.py
+++ handlers/_admin_delete.py
@@ -9,6 +9,6 @@ logger = logzero.setup_logger(__name__)
 
 
 @inject_quoted_sticker_id
-def admins_delete(_, update, quoted_sticker_id):
+def admin_delete(_, update, quoted_sticker_id):
     sticker_storage.delete_by_file_id_without_check(quoted_sticker_id)
     update.message.reply_text('Sticker have been deleted.')
